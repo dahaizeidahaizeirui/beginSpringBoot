ae0e0f16a64869b54ef56eafe54c2e3d04c1798a
EditYJ <158392613@qq.com>
2019-03-06 13:44:47 +0800

    启用redis缓存的基本配置
    启用redis缓存的基本配置

--- src/main/java/com/edityj/chapter2/Chapter2Application.java
+++ src/main/java/com/edityj/chapter2/Chapter2Application.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -19,6 +20,7 @@ import javax.annotation.PostConstruct;
         sqlSessionTemplateRef = "sqlSessionTemplate",
         annotationClass = Repository.class
 )
+@EnableCaching //启用缓存机制
 public class Chapter2Application {
 
     @Autowired
@@ -29,6 +31,7 @@ public class Chapter2Application {
         initRedisTemplate();
     }
 
+    //设置键值序列化器
     private void initRedisTemplate(){
         RedisSerializer serializer = redisTemplate.getStringSerializer();
         redisTemplate.setKeySerializer(serializer);
